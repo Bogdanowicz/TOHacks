c7ca0a9a072ae0d542a29ee46f7ac596b12e6bd6
Aidan Bird <aidan@birdwebwork.com>
2022-05-28 18:03:54 -0400

    modified:   .gitignore 	modified:   functions/index.js 	modified:   package-lock.json 	modified:   package.json

--- functions/index.js
+++ functions/index.js
@@ -3,14 +3,29 @@
 //   response.send("Hello from Firebase!");
 // });
 
+const cfg = require('./config');
+
 const funcs = require('firebase-functions');
-const admin = require('firebase-admin')
+const admin = require('firebase-admin');
 const express = require('express');
+
+const firebase = require('firebase');
+
+// const firebase = require('./node_modules/firebase/app')
+// const auth = require('./node_modules/firebase/auth')
+
+// import { initializeApp } from 'firebase/app';
+// import { getAuth, onAuthStateChanged, getRedirectResult } from 'firebase/auth';
+
+
 const app = express();
+const USER_TAB = 'users';
+
+console.log(cfg.getFirebaseConfig());
 
 admin.initializeApp();
-
-const USER_TAB = 'users';
+firebase.initializeApp(cfg.getFirebaseConfig());
+// initializeApp(cfg.getFirebaseConfig());
 
 app.get('/ping', (requ, resp) => { resp.send('Hello World'); });
 
@@ -45,6 +60,23 @@ app.get('/createUser', (requ, resp) => {
             resp.status(500).json({ error: 'error' });
             console.error(e)
         });
+});
+
+app.get('/signup', (requ, resp) => {
+    const newUser = {
+        email: requ.body.email,
+        password: requ.body.password,
+        confirmPassword: requ.body.confirmPassword,
+        handle: requ.body.handle,
+    };
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+    .then(data => {
+        return resp.status(201).json({message: `created user ${data.user.uid}`});
+    })
+    .catch(e => {
+        console.error(e);
+        return resp.status(500).json({ message: 'INTERNAL SERVER ERROR' });
+    });
 });
 
 exports.api = funcs.https.onRequest(app);

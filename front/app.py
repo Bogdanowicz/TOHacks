0621398eefa44af4b35ef24aa9660f436d33c646
Aidan Bird <aidan@birdwebwork.com>
2022-05-28 21:23:01 -0400

    added firebase json

--- front/app.py
+++ front/app.py
@@ -1,13 +1,21 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for, abort, make_response
+from flask import Flask, send_from_directory, render_template, request, redirect, url_for, abort, make_response
 
 # app configuration
-app = Flask(__name__, template_folder='templates',static_folder='css')
+app = Flask(__name__, template_folder='templates', static_folder='css')
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('base.html')
+
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+    if(request.method == 'POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)

fd2c9a5e895e7dc92af78b6c57626cce70548a8a
Aidan Bird <aidan@birdwebwork.com>
2022-05-28 22:56:10 -0400

    add local firebase

--- front/app.py
+++ front/app.py
@@ -14,8 +14,7 @@ def login():
     if(request.method == 'POST'):
         username = request.form.get('username')
         password = request.form.get('password')
-
-
+        curl 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA4i_9C1kw83UtFqwDpQPFzqM1Kor-1plc' -H 'Content-Type: application/json' --data-binary '{"email":"ab-test@example.com","password":"foobar","returnSecureToken":true}'
 
 if __name__ == "__main__":
     app.run(debug=True)
--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,23 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-app.get('/ping', (requ, resp) => { resp.send('Hello World'); });
+app.get('/ping', (requ, resp) => { 
+    const token = requ.body.idToken;
+
+    console.log(token);
+    admin.auth().verifyIdToken(token)
+        .then((decodedToken) => {
+            const uid = decodedToken.uid;
+            return admin.auth().getUser(uid)
+        })
+        .then((user) => {
+            return resp.status(201).json({ message: `${user.email}` });
+        })
+        .catch(e => {
+            console.error(e);
+            return resp.status(500).json({ message: 'INTERNAL SERVER ERROR' });
+        });
+});
 
 app.get('/getUser', (requ, resp) => {
     admin.firestore().collection(USER_TAB).get()
@@ -51,6 +67,12 @@ app.post('/signup', (requ, resp) => {
         confirmPassword: requ.body.confirmPassword,
         handle: requ.body.handle,
     };
+
+    if (newUser.email == undefined || newUser.password == undefined 
+        || newUser.confirmPassword !== newUser.password || newUser.handle == undefined) {
+        return resp.status(400).json({ handle: "BAD NEW USER INFO" });
+    }
+    console.log(newUser)
 
     db.doc(`/users/${newUser.handle}`).get()
         .then(doc => {
